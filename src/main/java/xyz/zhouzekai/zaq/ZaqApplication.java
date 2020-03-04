17c01a0581e17bbb64e57062cb1e55ce40a7c536
zkkdev <2657530327@qq.com>
2020-03-04 20:25:20 +0800

    chore(*): init springboot environment

---
+++ src/main/java/xyz/zhouzekai/zaq/ZaqApplication.java
@@ -0,0 +1,13 @@
+package xyz.zhouzekai.zaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/zhouzekai/zaq/ZaqApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.zhouzekai.zaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
